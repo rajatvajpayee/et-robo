3b909595ee592bf5a220aeac055f2a0d92d694a6
Rajat Vajpayee <rajatvajpayee@gmail.com>
2026-08-05 01:21:13 +0530

    shell scripts to run best model

---
+++ problem1_mnist/test_best_config.sh
@@ -0,0 +1,7 @@
+export PYTHONPATH=.
+
+echo "=== testing ==="
+CUDA_VISIBLE_DEVICES=7 python test_main.py \
+--checkpoint ./checkpoints/best_model.pth \
+--test_cfg configs/training.yaml \
+--model_cfg configs/model.yaml
